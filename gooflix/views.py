4a516b6586bef70c10de944b3b6bb6c45531839f
Allan Assis <allan.andrade@g.globo>
2020-04-18 16:48:46 -0300

    Add view to get titles from netflix

--- gooflix/views.py
+++ gooflix/views.py
@@ -1,5 +1,22 @@
-from aiohttp import web
+import aiohttp_jinja2
+
+from request import get_titles
 
 
+@aiohttp_jinja2.template('index.html')
 async def index(request):
-    return web.Response(text='Hello Aiohttp!')
+    cookies = dict(request.cookies)
+    has_cookies = cookies.get("NetflixId", False) and cookies.get('SecureNetflixId', False)
+    return {"cookies": has_cookies}
+
+
+@aiohttp_jinja2.template('index.html')
+async def titles(request):
+    data = {**dict(request.query), **dict(request.cookies)}
+    netflix_id = data['NetflixId']
+    secure_netflix_id = data['SecureNetflixId']
+    query = data['Query']
+
+    titles = get_titles(query, netflix_id, secure_netflix_id)
+
+    return {"titles": titles, "cookies": True}
