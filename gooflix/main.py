88d0069bebfd8ffe44e6cc035d7c60c610212428
Allan Assis <allan.andrade@g.globo>
2020-04-18 16:48:14 -0300

    setup static folder

--- gooflix/main.py
+++ gooflix/main.py
@@ -6,6 +6,13 @@ from aiohttp import web
 
 from routes import setup_routes
 
+
+def setup_static_routes(app):
+    app.router.add_static('/static/',
+                          path=str(BASE_DIR / 'gooflix' / 'static'),
+                          name='static')
+
+
 app = web.Application()
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
@@ -16,5 +23,6 @@ aiohttp_jinja2.setup(
     loader=jinja2.FileSystemLoader(str(BASE_DIR / 'gooflix' / 'templates'))
     )
 
+setup_static_routes(app)
 setup_routes(app)
 web.run_app(app)
