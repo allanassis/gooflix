d719cc0cd6281cca91e97cf674140d7728e2cb65
Allan Assis <allan.andrade@g.globo>
2020-04-18 16:47:57 -0300

    setup jinja

--- gooflix/main.py
+++ gooflix/main.py
@@ -1,6 +1,20 @@
+import pathlib
+
+import aiohttp_jinja2
+import jinja2
 from aiohttp import web
+
 from routes import setup_routes
 
 app = web.Application()
+
+BASE_DIR = pathlib.Path(__file__).parent.parent
+
+
+aiohttp_jinja2.setup(
+    app,
+    loader=jinja2.FileSystemLoader(str(BASE_DIR / 'gooflix' / 'templates'))
+    )
+
 setup_routes(app)
 web.run_app(app)
